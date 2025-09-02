dccf4eb953e15382e85b37e19c2f43b4ecdc516d
Hanbanan <hannah.watkins@ucas-edu.net>
2025-09-02 14:39:25 -0600

    finished variable assignment

---
+++ practices/variables_practice.py
@@ -0,0 +1,22 @@
+# HW 7thy variable practice
+
+Name = "My name is Hannah" 
+print(Name)
+Num_10 = "7" 
+print(Num_10)
+Num_1000 = "234" 
+print (Num_1000)
+breakfast = "I had an asiago bagel"
+print (breakfast)
+color = "purple"
+print (color)
+school = "UCAS"
+print (school)
+year = "2025"
+print (year)
+eyes = "Blue"
+print (eyes)
+age = "14"
+print(age)
+subject = "history"
+print (subject)
