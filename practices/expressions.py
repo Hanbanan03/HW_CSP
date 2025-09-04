3211b46091141c09f23511742ccdcd53e8df60b9
Hanbanan <hannah.watkins@ucas-edu.net>
2025-09-04 14:40:51 -0600

    expressions notes

---
+++ practices/expressions.py
@@ -0,0 +1,36 @@
+# HW 7th expression notes
+
+# what is an algorithm?
+    # Just a set of instuction to complete a task
+name = input("Hello, what is your name:\n")
+print("Hello", name)
+
+# List steps in an algorithm
+# 1. Declare variables(get needed info)
+# 2. Do the equations- Do the things
+# 3. Display results
+# Treyson has 12 apples, he has 5 friends that he wants to give apples to. How many apples does each friend get? 
+apples = 12
+friends =5
+print("Each friend gets", apples/friends, "apples!")
+
+
+# List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+num_1=12
+num_2=7
+
+print("Subtrction(-): ", num_1-num_2)
+print("Addition(+): ", num_1+num_2)
+print("Multiplication(*): ", num_1*num_2)
+print("Division(/): ", num_1/num_2) #floats are numbers with
+print("exponents(**): ", num_1**num_2)
+print("Interger division(//): ", num_1//num_2) #will only give you the whole number of a division equation
+print("Modulo(%): ", num_1%num_2) #gives the remainder
+
+#average age of a group of 4 people
+age=3
+age2=29
+age3=10
+age4=18
+print("The average is", (age+age2+age3+age4)/4)
+
