c7e1c3cbc9187795d454baa7faf83d6e33997258
Hanbanan <hannah.watkins@ucas-edu.net>
2025-09-05 14:42:20 -0600

    started on other asignments

---
+++ practices/budget calculator.py
@@ -0,0 +1,5 @@
+# HW 7th financial calculator
+
+# -income, rent, utilites, groceries, transportation savings is 10%*income
+#spend=income-(utilites+rent-groceries+transportation+savings)
+#percent_=rent/income*100
