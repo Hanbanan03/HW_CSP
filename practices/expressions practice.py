58797f8cb6c8d96fdbf191b5b76f96e0579b3dea
Hanbanan03 <hannah.watkins@ucas-edu.net>
2025-09-07 04:52:02 +0000

    finished expressions practice

---
+++ practices/expressions practice.py
@@ -0,0 +1,20 @@
+# HW 7th expressions practice
+
+result = 7-24/8*4+6
+print(result)
+result = 18/3-7+2*5
+print(result)
+result = 6*4/12+72/8-9
+print(result)
+result = (17-6/2)+4*3
+print(result)
+result = -2*(1*4-2/2)+(6+2-3)
+print(result)
+result = -1*((3-4*7)/5)-2*24/6
+print(result)
+result = (3*(5**2)/15)-(5-(2**2))
+print(result)
+result = (1**4*2**2+3**3)-2**5/4
+print(result)
+result = (22/2-2*5)**2+(4-6/6)**2
+print(result)
--- practices/first_python_program.py
+++ practices/first_python_program.py
@@ -1,3 +1,3 @@
-name = "Hannah";
+name = "hannah";
 
 print("Hello "+ name + "!")
