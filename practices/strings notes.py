768e53480a9e1156f3dab1583cbf907cd096fa0f
Hanbanan <hannah.watkins@ucas-edu.net>
2025-09-08 14:41:43 -0600

    worked on strings

--- practices/strings notes.py
+++ practices/strings notes.py
@@ -1,3 +1,21 @@
 # HW 7th strings notes
 
-print("I did it")
+#What makes something a string? a set of symbols held togethr by quotation marks
+#Why do we have strings? 
+#How do stupid proofing and sanitization relate to each other?
+#What is debugging?
+#How do you debug the different types of errors?
+#Describe what each of the methods below does:
+#find()
+#concatenate (add) 
+#upper()
+#lower()
+#strip()
+# Examples
+first_name = input("What is your first name:\n")
+#.strip( ).lower().upper().capitalize()
+last_name=input("what is your last name")
+
+sentence = 'The quick brown fox jumps over the lazy dog.'
+
+print("Welcome to my porgram", first_name, last_name "!")
