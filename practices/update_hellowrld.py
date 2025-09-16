2b345141b256f99e9f0467bcb7fb7cf4d659f8fb
Hanbanan <hannah.watkins@ucas-edu.net>
2025-09-16 14:37:07 -0600

    updated hello world

---
+++ practices/update_hellowrld.py
@@ -0,0 +1,16 @@
+# HW 7th update hello wrld
+
+
+def Hello(name):
+    print(f"Hello {name}")
+    
+name = input("What is your name\n")
+Hello("Hannah")
+
+Hello("Enzo")
+
+Hello("Jovie")
+
+Hello("Celeste")
+
+Hello("Eva")
