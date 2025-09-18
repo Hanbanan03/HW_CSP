2f51d906f8b1b296f9b66835bace68f281d1b623
Hanbanan <hannah.watkins@ucas-edu.net>
2025-09-18 14:21:28 -0600

    finished notes and started old enough

--- Notes/conditionals notes.py
+++ Notes/conditionals notes.py
@@ -1,20 +1,20 @@
 # HW 7th notes
 
-#What puts something inside of the “if” statement?-condition or expression that follows if
+#What puts something inside of the “if” statement?-tab
 #homework_done = input("Is your homework done:").strip().capitalize()
 #if homework_done == "No":
-    print("Then go do your homework!")
+#print("Then go do your homework!")
 #else:
    # print("Yes, you can go")
 
-grade= 84
+#grade= 84
 
-if grade > 89:
-    print(f"You have {grade}% that is an A!")
-elif grade > 79:
-    print(f"you have {grade}% that is a B")
-else:
-    print(f"You have a {grade}% that is a C!")
+#if grade > 89:
+    #print(f"You have {grade}% that is an A!")
+#elif grade > 79:
+   # print(f"you have {grade}% that is a B")
+#else:
+    #print(f"You have a {grade}% that is a C!")
 #What do if statements do? check if something is true or false
 #What are boolean statements? always true or false
 #What do else statements do? specifies a block of code
@@ -26,6 +26,15 @@ else:
 #>=greater than or equal to
 #== - are they equal
 #! not equal to
-#What are the 3 logical operators?
-#What are logical operators for?
-#What does a nested conditional statement do?
+#What are the 3 logical operators? and, or, not
+
+name= input("What is your name: ")
+
+if name== "Ms Larose":
+    print("You are the teacher!")
+elif name== "Tia" or name == "Ashley":
+    print("You are the TA")
+else:
+    print(f"Hello {name}, you are a student!")
+#What are logical operators for? can be used to make decisions in code or evaluate complex statements.
+#What does a nested conditional statement do? a condition that only happens inside another one
