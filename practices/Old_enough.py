026b321639235bdc340fae519f41a933200ac3f5
Hanbanan03 <hannah.watkins@ucas-edu.net>
2025-09-18 20:43:45 -0600

    Update Old_enough.py
    worked on old enough

--- practices/Old_enough.py
+++ practices/Old_enough.py
@@ -4,3 +4,7 @@ age = int(input("How old are you?: "))
 
 if age <= 4:
     print("You can go to school!")
+elif age <= 15:
+    print("You can get a learners permit!)
+else age <= 16:
+    print("You can drive!")
