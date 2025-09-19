60cfea8af426d6456b94fd7a982f829ff18ffb69
Hanbanan <hannah.watkins@ucas-edu.net>
2025-09-19 14:04:24 -0600

    time notes

---
+++ Notes/time_notes.py
@@ -0,0 +1,15 @@
+# HW 7th about getting the time
+
+import time
+import datetime
+
+epoch=time.time()
+readable_time = time.ctime(epoch)
+
+print(f"The in seconds time since Jan 1, 1970 {epoch} ")
+print(f"The time is: {readable_time}")
+hour = now.hour
+
+now = datetime.datetime.now()
+print(f"The time is {now}")
+print(f"The hour is {hour}")
