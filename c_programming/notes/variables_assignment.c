f3e0e9e24af2743114b188c94ed38364f30fcc94
Hanbanan03 <hannah.watkins@ucas-edu.net>
2025-10-08 20:09:27 +0000

    finished variables

---
+++ c_programming/notes/variables_assignment.c
@@ -0,0 +1,31 @@
+//HW 7th variables assignment
+
+
+#include <stdio.h>
+
+int main() {
+
+    char name[] = "Hannah";
+    int luckyNumber = 22;
+    float gpa = 3.6;
+    char breakfast[] = "muffin";
+    char favoriteColor[] = "purple";
+    char schoolName[] = "UCAS";
+    int currentYear = 2025;
+    char eyeColor[] = "blue";
+    int age = 15;
+    char favoriteSubject[] = "Biology";
+
+    printf("Name: %s\n", name);
+    printf("Lucky Number (1-10): %d\n", luckyNumber);
+    printf("GPA: %.1f\n", gpa);
+    printf("Breakfast: %s\n", breakfast);
+    printf("Favorite Color: %s\n", favoriteColor);
+    printf("School Name: %s\n", schoolName);
+    printf("Current Year: %d\n", currentYear);
+    printf("Eye Color: %s\n", eyeColor);
+    printf("Age: %d\n", age);
+    printf("Favorite Subject: %s\n", favoriteSubject);
+
+    return 0;
+}
