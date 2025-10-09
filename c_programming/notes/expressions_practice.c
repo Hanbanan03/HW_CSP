3838dca556bb33b5d937d8aa3be04af7c9340b98
Hanbanan03 <hannah.watkin@ucas-edu.net>
2025-10-09 14:24:07 -0600

    finished stings notes

---
+++ c_programming/notes/expressions_practice.c
@@ -0,0 +1,16 @@
+// HW 7th expressions practice
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int num_4=4;
+    int num_8=8;
+
+    printf("7-24/num_8*num_4+6 = %d\n", 7-24 / num_8 * num_4+6);
+    printf("18/3-7+2*5 = %d\n", 18/3 - 7 + 2 * 5);
+    printf("6*num_4/12+72/num_8-9 = %d\n", 6*num_4/12+72/num_8-9);
+    printf("(17-6/2)+num_4*3=%d\n", (17 - 6 / 2) + num_4 * 3);
+    printf("-2*(1*num_4-2/2)+(6+2-3) = %d\n", -2 * (1 * num_4 - 2 / 2) + (6 + 2 - 3));
+    printf("(3*5**2/15)-(5-2**2) = %d\n", (3.0 * pow(5, 2) / 15.0) - (5.0 - pow(2, 2)));
+}
