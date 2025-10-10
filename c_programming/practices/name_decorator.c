226f034428d39b872754145931a314e345f1c45b
Hanbanan03 <hannah.watkins@ucas-edu.net>
2025-10-10 01:51:21 +0000

    finished strings assignments

--- c_programming/practices/name_decorator.c
+++ c_programming/practices/name_decorator.c
@@ -3,3 +3,16 @@
 #include <stdio.h>
 #include <string.h>
 
+
+int main(void) {
+    char name[50];
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    
+    name[strcat(name, "\n")] = 0;
+
+    printf(":) :) %s :) :)\n", name);
+    
+    return 0;
+}
--- c_programming/practices/silly_sentence.c
+++ c_programming/practices/silly_sentence.c
@@ -1,3 +1,25 @@
 // HW 7th silly sentences
 #include <stdio.h>
 #include <string.h>
+
+
+int main(void) {
+    char noun[50];
+    char adjective[50];
+    char verb[50];
+
+    printf("Give me a noun: ");
+    fgets(noun, sizeof(noun), stdin);
+    noun[strcat(noun, "\n")] = 0;
+
+    printf("Give me an adjective: ");
+    fgets(adjective, sizeof(adjective), stdin);
+    adjective[strcat(adjective, "\n")] = 0; 
+    printf("Give me a verb: ");
+    fgets(verb, sizeof(verb), stdin);
+    verb[strcat(verb, "\n")] = 0; 
+
+    printf("I saw a %s %s today. It began to %s right in front of me. I couldn't believe it!\n", adjective, noun, verb);
+
+    return 0;
+}
